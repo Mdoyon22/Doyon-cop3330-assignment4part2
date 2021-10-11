f02e7052ee24e97750451383df1b6725c506d1ac
Michael <michaeldoyon1@gmail.com>
2021-10-11 15:49:30 -0400

    Initial Commit

---
+++ src/main/java/ucf/assignments/App.java
@@ -0,0 +1,40 @@
+
+
+
+package ucf.assignments;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+
+public class App extends Application
+{
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage)
+    {
+        Parent root = null;
+
+        try
+        {
+            root = FXMLLoader.load(getClass().getResource("App.fxml"));
+
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("To do list!");
+            primaryStage.show();
+        }
+
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
