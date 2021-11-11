7b2c7b9ae004de4db045eb2528aa4bb2f95d5782
Michael <michaeldoyon1@gmail.com>
2021-11-10 22:21:17 -0500

    Initial Commit

--- src/main/java/ucf/assignments/Controller.java
+++ src/main/java/ucf/assignments/Controller.java
@@ -5,141 +5,61 @@
 
 
 package ucf.assignments;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import java.util.ArrayList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import javafx.util.StringConverter;
 
-public class Controller
+import java.io.*;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
+import java.util.ResourceBundle;
+
+
+public class Controller implements Initializable
 {
-    //Declare and initialize AllLists arraylist by calling it
-    //Utilize and interpret AllLists methods according to eventhandlers
+    //Create a list object
+    ToDoList list = new ToDoList();
 
-    //event handlers code
+    //Global observable list
+    ObservableList<Item> obList = FXCollections.observableArrayList();
 
+    //Label that shows file name when loaded
+    @FXML private Label fileLabel;
 
-    //Save and load options
-    public void saveListClicked(ActionEvent actionEvent)
-    {
-        //Get that list that was clicked - saveList
-        //Call AllLists listExists(), if true continue, else addToDoList() to AllLists arraylist
-        //Call AllLists save(saveList) method by passing the saveList parameter
-    }
+    //Vertical box for "save" and "load" buttons
+    @FXML private VBox fileBox;
 
-    public void saveAllClicked(ActionEvent actionEvent)
-    {
-        //Call AllLists saveAll() method
-    }
+    //Global fileChooser for "save" and "load"
+    FileChooser fileChooser = new FileChooser();
 
-    public void loadRecentClicked(ActionEvent actionEvent)
-    {
-        //Get the ToDoList object LoadList from calling AllLists loadRecent() method
-        //display() that ToDoList object LoadList
-    }
+    //Dropdown combobox for display options
+    @FXML private ComboBox<String> displayOptionsBox;
 
-    public void loadAllClicked(ActionEvent actionEvent)
-    {
-        //Declare new arraylist LoadAll
-        //Initialize that arraylist LoadAll with return value from calling AllLists loadAll() method
-        //Pass that new arraylist into display() method
-    }
-
-    //item options
-    public void addItemClicked(ActionEvent actionEvent)
-    {
-        //Show up the anchorPane "item_Pane"
-    }
-
-    public void saveItemClicked(ActionEvent actionEvent)
-    {
-        //Get the list of the saved item
-        //Call AllLists's listExists(), if true continue, else addToDoList() to AllLists arraylist
-        //Get the description "item_des" and date "item_date" text fields
-        //Create new Item object by calling the constructor
-        //Add the new Item object into the existing list
-        //Call ToDoList addItem() method
-    }
-
-    public void editItemClicked(ActionEvent actionEvent)
-    {
-        //Get the ToDoList list and Item item objects from which button clicked
-        //Call AllLists's listExists(), if true continue, else addToDoList() to AllLists arraylist
-        //If description edited, call the Item object's editDescription() method
-        //If date edited, call the Item's object's editDate() method
-    }
-
-    public void deleteItemClicked(ActionEvent actionEvent)
-    {
-        //Get the ToDoList list and Item item objects from which button clicked
-        //Call AllLists's listExists(), if true continue, else addToDoList() to AllLists arraylist
-        //Remove the item from that ToDoList object's items list
-    }
-
-    public void itemFinishedSelected(ActionEvent actionEvent)
-    {
-        //Get the Item and ToDoList object from which the finished is selected
-        //Call AllLists's listExists(), if true continue, else addToDoList() to AllLists arraylist
-        //Check whether it is checked or not
-        //If checked mark the boolean "check" as true, else as false
-        //Update that item finished by calling editFinished() with passing in the boolean "check"
-    }
-
-    public void editListClicked(ActionEvent actionEvent)
-    {
-        //Get the ToDoList list object from which the edit is clicked
-        //Call AllLists's listExists(), if true continue, else addToDoList() to AllLists arraylist
-        //Find where the list is in (using the title) from AllLists arraylist
-        //If title is edited, call the ToDoList object's editTitle() by passing new title
-    }
-
-    public void deleteListClicked(ActionEvent actionEvent)
-    {
-        //Get the ToDoList list object from which the edit is clicked
-        //Call AllLists's listExists(), if true continue, else addToDoList() to AllLists arraylist
-        //Find where the list is in (using the title) from AllLists arraylist
-        //Remove that list from the AllLists arraylist
-    }
-
-    public void addListClicked(ActionEvent actionEvent)
-    {
-        //Show up the anchor pane "todoList_Pane"
-    }
-
-    //Display options selection
-    public void allDisplaySelected(ActionEvent actionEvent)
-    {
-        //Get the ToDoList title from 'displayToDoList' text field for display options
-        //Find the ToDoList by using getTitle() and listExists() and store it in new ToDoList object - displayList
-        //Check whether allDisplay is marked or not
-        //If it is, call the displayItems() method with the parameter of the
-        //return value from ToDoLists's getAllItems() method by passing displayList
-    }
-
-    public void completeDisplaySelected(ActionEvent actionEvent)
-    {
-        //Get the ToDoList title from 'displayToDoList' text field for display options
-        //Find the ToDoList by using getTitle() and listExists() and store it in new ToDoList object - displayList
-        //Check whether completeDisplay is marked or not
-        //If it is, call the displayItems() method with the parameter of the return value from ToDoLists's completeItems() method by passing displaylist
-    }
-
-    public void incompleteDisplaySelected(ActionEvent actionEvent)
-    {
-        //Get the ToDoList title from 'displayToDoList' text field for display options
-        //Find the ToDoList by using getTitle() and listExists() and store it in new ToDoList object - displayList
-        //Check whether inCompleteDisplay is marked or not
-        //If it is, call the displayItems() method with the parameter of the return value from ToDoLists's inCompleteItems() method by passing displayList
-    }
-
-    public void displayItems(ArrayList<Item> items, ToDoList list)
-    {
-        //Find the 'list' and clear its display
-        //Loop through 'items'
-        //Display them on screen with the 'list'
-    }
-
-    public void displayLists(ArrayList<ToDoList> lists)
-    {
-        //First call the AllLists's sort() method
-        //Then loop through the parameter of all lists
-        //Display them on screen
-    }
+    //Configuring table and table columns
+    @FXML
+    private TableView<Item> tableView;
+    @FXML
+    private TableColumn<Item, String> descriptionColumn;
+    @FXML
+    private TableColumn<Item, LocalDate> dueDateColumn;
+    @FXML
+    private TableColumn<Item, CheckBox> completedColumn;
 }
